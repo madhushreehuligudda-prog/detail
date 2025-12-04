ee624977144b59b4a5f7c3bea93fc6bf12409857
madhushreehulligudda <madhushreehuligudda@gmail.com>
2025-12-04 13:11:30 +0530

    Replace product test with employee details test

--- test_employee.py
+++ test_employee.py
@@ -1,17 +1,9 @@
-from product import format_product_info
-
-def test_format_product_info():
-    product_id = 101
-    name = "Laptop"
-    quantity = 5
-    price = 799.99
-
-    expected_output = (
-        "Product Information:\n"
-        "ID: 101\n"
-        "Name: Laptop\n"
-        "Quantity: 5\n"
-        "Price: $799.99"
+from employee import employee_details
+def test_employee_details():
+    output=(
+        "Employee_Name:Alice\n"
+        "Employee_ID:E1001\n"
+        "Department:IT\n"
+        "Salary:55000"
     )
-
-    assert format_product_info(product_id, name, quantity, price) == expected_output
+    assert employee_details("Alice","E1001","IT",55000) == output
